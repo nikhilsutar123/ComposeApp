e4089b5845cf2d1285bff019522875ae77ccac8f
nikhil.sutar <nikhil.sutar@viraltech.in>
2024-08-25 12:27:46 +0530

    textfield, button & snackbar in compose

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.composeapp
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text2.input.TextFieldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,56 +34,51 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                Modifier
-                    .fillMaxSize()
-
+            val snackbarHostState = remember {
+                SnackbarHostState()
+            }
+            val scope = rememberCoroutineScope()
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+            Scaffold(
+                modifier = Modifier.fillMaxSize().padding(8.dp),
+                snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
             ) {
-                val names = listOf("Alice", "Bob", "Charlie", "David", "Eve")
-                val selectedName = remember {
-                    mutableStateOf(names[0])
-                }
-                NameBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize(),
-                    names,
-                ) {
-                    selectedName.value = it
-                }
-                Box(
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
-                        .background(Color.Magenta)
-                        .weight(1f)
-                        .fillMaxSize(), contentAlignment = Alignment.Center
+                        .fillMaxSize()
+                        .padding(it)
                 ) {
-                    Text(text = selectedName.value, fontSize = 60.sp, textAlign = TextAlign.Center)
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = textFieldState,
+                        onValueChange = { value ->
+                            textFieldState = value
+                        },
+                        label = { Text(text = "Enter name") }, singleLine = true
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(onClick = {
+                        scope.launch {
+                                snackbarHostState.showSnackbar(message = "Hello $textFieldState!")
+                        }
+                    }, modifier = Modifier.align(Alignment.End)){
+                        Text("Click me!")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun NameBox(
-    modifier: Modifier = Modifier,
-    names: List<String>,
-    updateName: (String) -> Unit,
-) {
-    Box(modifier = modifier
-        .background(Color.Cyan)
-        .clickable {
-            updateName(
-                names.random()
-            )
-        }, contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Hello", fontSize = 60.sp, textAlign = TextAlign.Center)
-    }
-}
