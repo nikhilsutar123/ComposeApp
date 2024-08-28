8ae684ba784260113722ad26ca40f4d7dcc126f1
nikhil sutar <nikhil.sutar@viraltech.in>
2024-08-28 12:19:08 +0530

    launchedEffect in compose

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.composeapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -19,39 +33,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val constraints = ConstraintSet() {
-                val greenBox = createRefFor("greenbox")
-                val redBox = createRefFor("redbox")
-                val guideLineTop = createGuidelineFromTop(0.5f)
-                val guideLineStart = createGuidelineFromStart(0.5f)
-                constrain(greenBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                constrain(redBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(greenBox.end)
-                    end.linkTo(parent.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                createHorizontalChain(greenBox,redBox, chainStyle = ChainStyle.Spread)
+            WithLaunchedEffect()
+        }
+    }
+
+    @Composable
+    fun WithLaunchedEffect() {
+        var counter = remember {
+            mutableIntStateOf(0)
+        }
+        val context = LocalContext.current
+        LaunchedEffect(key1 = true) {
+            Toast.makeText(context, "Hello", Toast.LENGTH_SHORT).show()
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = { counter.intValue++ }) {
+                Text(text = "tap me!")
             }
-
-            ConstraintLayout(constraintSet = constraints, modifier = Modifier.fillMaxSize()) {
-                Box(modifier = Modifier
-                    .background(Color.Cyan)
-                    .layoutId("greenbox")) {
-
-                }
-                Box(modifier = Modifier
-                    .background(Color.Red)
-                    .layoutId("redbox")) {
-
-                }
-            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Text("count ${counter.intValue}")
         }
     }
 }
