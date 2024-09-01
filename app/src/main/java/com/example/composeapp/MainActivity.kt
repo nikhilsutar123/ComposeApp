434d4ee199b03b3309709170331ff22c52b5e731
nikhil.sutar <nikhil.sutar@viraltech.in>
2024-09-01 23:05:14 +0530

    disposable effect in jetpack compose

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.composeapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,23 +21,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WithLaunchedEffect()
+           AirplaneModeScreen()
         }
     }
 
@@ -56,6 +54,41 @@ class MainActivity : ComponentActivity() {
             }
             Spacer(modifier = Modifier.height(10.dp))
             Text("count ${counter.intValue}")
+        }
+    }
+
+    @Composable
+    fun AirplaneModeScreen() {
+        var data by remember {
+            mutableStateOf("No state")
+        }
+        val context = LocalContext.current
+        val broadcastReceiver = remember {
+            object : BroadcastReceiver() {
+                override fun onReceive(p0: Context?, p1: Intent?) {
+                    Log.d("disposable", "AirplaneModeScreen:onReceive called...")
+                    val bundle = p1?.getBooleanExtra("state", false) ?: return
+                    data = if (bundle)
+                        "Airplane mode enabled"
+                    else
+                        "Airplane mode disabled"
+                }
+
+            }
+        }
+        DisposableEffect(key1 = true) {
+            Log.d("disposable", "AirplaneModeScreen:entered DisposableEffect...")
+            val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            context.applicationContext.registerReceiver(broadcastReceiver,intentFilter)
+            Log.d("disposable", "AirplaneModeScreen: broadcast registered")
+            onDispose {
+                context.applicationContext.unregisterReceiver(broadcastReceiver)
+                Log.d("disposable", "AirplaneModeScreen: broadcast unregistered")
+            }
+        }
+
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "Status: $data")
         }
     }
 }
