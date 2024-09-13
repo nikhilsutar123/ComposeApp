d633f3cc50e909b2bef09baf397a370faf296b5b
nikhil sutar <nikhil.sutar@viraltech.in>
2024-09-13 16:22:29 +0530

    basic animation

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -9,12 +9,29 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseInOutBounce
+import androidx.compose.animation.core.EaseInQuad
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +41,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -40,15 +58,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            val value = produceStateDemo(count = 10)
-            derivedStateDemo()
-//            Column(
-//                modifier = Modifier.fillMaxSize(),
-//                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.Center
-//            ) {
-//                Text(text = "${value.value}", style = TextStyle(fontSize = 100.sp))
-//            }
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(targetValue = sizeState, spring(Spring.DampingRatioHighBouncy))
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Magenta,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(3000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(color = color)
+                    .padding(8.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Column {
+                    Button(onClick = {
+                        sizeState += 50.dp
+                    }) {
+                        Text("grow!")
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(onClick = {
+                        sizeState -= 50.dp
+                    }) {
+                        Text("shrink!")
+                    }
+                }
+            }
         }
     }
 
@@ -63,17 +106,17 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun derivedStateDemo(){
+    fun derivedStateDemo() {
         var count by remember {
             mutableIntStateOf(0)
         }
         val counterText by remember {
             derivedStateOf {
-              "counter is $count"
+                "counter is $count"
             }
         }
 
-        Button(onClick = { count++}) {
+        Button(onClick = { count++ }) {
             Text(text = counterText)
         }
     }
