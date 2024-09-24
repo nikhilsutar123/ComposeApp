32d0ab61479aefa76cd4cde145cb1e2a4e0c1167
nikhil sutar <nikhil.sutar@viraltech.in>
2024-09-24 13:05:30 +0530

    UI for circular progress bar

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,15 +29,26 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressBar(percentage = 0.8f, number = 100, radius = 100.dp, strokeWidth = 5.dp)
+            }
+
         }
     }
 
@@ -48,22 +61,37 @@ class MainActivity : ComponentActivity() {
         strokeWidth: Dp = 8.dp,
         color: Color = Color.Green,
         fontSize: Dp = 16.dp,
-        radius: Int
+        radius: Dp = 50.dp
     ) {
         var animPlayed by remember {
             mutableStateOf(false)
         }
 
         val currPercent by animateFloatAsState(
-            targetValue = if (animPlayed) percentage else 0f,
-            animationSpec = tween(
-                durationMillis = animDuration,
-                delayMillis = animDelay
+            targetValue = if (animPlayed) percentage else 0f, animationSpec = tween(
+                durationMillis = animDuration, delayMillis = animDelay
             )
         )
 
         LaunchedEffect(key1 = true) {
             animPlayed = true
+        }
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.size(radius * 2f)) {
+            Canvas(modifier = Modifier.size(radius * 2f)) {
+                drawArc(
+                    color = color,
+                    startAngle = -90f,
+                    sweepAngle = 360 * currPercent,
+                    useCenter = false,
+                    style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+                )
+            }
+            Text(
+                text = "${(currPercent * number).toInt()}%",
+                color = Color.Black,
+                fontSize = TextUnit(value = fontSize.value, type = TextUnitType.Sp),
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 
@@ -158,10 +186,8 @@ class MainActivity : ComponentActivity() {
                 override fun onReceive(p0: Context?, p1: Intent?) {
                     Log.d("disposable", "AirplaneModeScreen:onReceive called...")
                     val bundle = p1?.getBooleanExtra("state", false) ?: return
-                    data = if (bundle)
-                        "Airplane mode enabled"
-                    else
-                        "Airplane mode disabled"
+                    data = if (bundle) "Airplane mode enabled"
+                    else "Airplane mode disabled"
                 }
 
             }
